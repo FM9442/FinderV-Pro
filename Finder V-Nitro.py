d71d526a18ef6bd16d861e82a6ca9387688b2e51
FM <82032202+FM9442@users.noreply.github.com>
2021-12-28 02:03:08 +0100

    Added Color, modules and loop

--- Finder V-Nitro.py
+++ Finder V-Nitro.py
@@ -1,9 +1,34 @@
-# Version 0.2
-import time, os, sys
+#Made by Lincoln#6812 aka github.com/FM9442
+# Version 0.3
+import time, os, sys, lxml, requests, json, duckduckgo_search
+from serpapi import GoogleSearch
+from duckduckpy import query
+from selenium import webdriver
+from urllib.parse import urlencode, urlunparse
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from colorama import init
+from colorama import Fore, Back, Style
+init()
 
+print(Fore.LIGHTRED_EX + "Bienvenue sur Finder V-Nitro!")
+print(Fore.BLACK + "https://github.com/FM9442/FinderV-Pro")
+print(Fore.BLUE + "Discord : Lincoln#6812")
+print(Style.RESET_ALL)
+time.sleep(1)
+print("")
+input("Press Enter to launch Finder V-Nitro...")
+os.system("cls")
+print("Launched!")
+time.sleep(1)
+os.system("cls")
+
+Lastname=input(Fore.MAGENTA + "Nom de famille précis : ")
+City=input(Fore.MAGENTA + "Ville : ")
+print(Style.RESET_ALL)
 
 # Performing google search using Python code
-version = ("0.2")
+version = ("0.3")
 class Gsearch_python:
    def __init__(self,name_search):
       self.name = name_search
@@ -17,33 +42,56 @@ class Gsearch_python:
          count += 1
          print (count)
          print(i + '\n')
-print("V-Nitro " + version + " running on ",sys.version)
-time.sleep(1.5)
+print(Fore.BLUE + "V-Nitro " + version + " running on ",sys.version)
+print(Style.RESET_ALL)
 
-Lastname=input("Nom de famille précis : ")
-City=input("Ville : ")
-
-
+print(Fore.BLUE + "G" + Fore.RED + "o" + Fore.YELLOW + "o" + Fore.BLUE + "g" + Fore.GREEN + "l" + Fore.RED + "e")
+print(Style.RESET_ALL)
 if __name__=='__main__':
    gs = Gsearch_python(Lastname + City)
    gs.Gsearch()
 
-anwser=input("Refaire une recherche Y/N : ")
+time.sleep(2)
+print(Fore.CYAN + "Bing")
+print("Non disponible")
+#Using Bing
+#query=Lastname + City
+#url = urlunparse(("https", "www.duckduckgo.com", "/", "", urlencode({"q": query}), ""))
+#custom_user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
+#req = Request(url, headers={"User-Agent": custom_user_agent})
+#page = urlopen(req)
+# Further code I've left unmodified
+#soup = BeautifulSoup(page.read())
+#links = soup.findAll("a")
+#for link in links:
+#    print(link["href"])
+print(Style.RESET_ALL)
+
+time.sleep(2)
+print(Fore.CYAN + "Yahoo")
+print("Non disponible")
+
+time.sleep(2)
+print(Fore.CYAN + "DuckDuckGo")
+print("Non disponible")
+
+
+anwser=input(Fore.MAGENTA + "Refaire une recherche Y/N : ")
 yesanwser="Y"
 noanwser="N"
 os.system("cls")
 
 while anwser == yesanwser:
-    print("V-Nitro " + version + " running on ",sys.version)
+    print(Fore.BLUE + "V-Nitro " + version + " running on ",sys.version)
 
     time.sleep(1.5)
-    Lastname=input("Nom de famille précis : ")
-    City=input("Ville : ")
+    Lastname=input(Fore.MAGENTA + "Nom de famille précis : ")
+    City=input(Fore.MAGENTA + "Ville : ")
     if __name__=='__main__':
         gs = Gsearch_python(Lastname + City)
         gs.Gsearch()
     
     anwser=input("Refaire une recherche Y/N : ")
-
-os.system("cls")    
+print(Style.RESET_ALL)
+os.system("cls")
 print("Bye!")
